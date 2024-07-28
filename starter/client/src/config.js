a28c602df679123a3e12f02db3063e7de6f04fdf
DatTT0706 <trantatdat.07.06.2001@gmail.com>
2024-07-28 23:32:57 +0700

    Implement todo app

---
+++ starter/client/src/config.js
@@ -0,0 +1,4 @@
+export const AUTH0_DOMAIN_NAME=`dev-83tpmf4hijs8offu.us.auth0.com`
+export const AUTH0_CLIENT_ID=`4vFsQbsv3KeVNEs7J3rZzxavv5TS33Eu`
+export const AUTH0_AUDIENCE=`https://dev-83tpmf4hijs8offu.us.auth0.com/api/v2/`
+export const API_DESTINATION=`https://vlmpg7g8y1.execute-api.us-east-1.amazonaws.com/dev`
